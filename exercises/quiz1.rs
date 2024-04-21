f3dcda378885a355088fbf0c78a90be9e638e03e
Ryan Khetlyr <ryan_khetlyr@waed.uscourts.gov>
2024-04-21 11:35:25 -0700

    up to arrays in primitive types

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,11 +13,14 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+ fn calculate_price_of_apples( apples: u32) -> u32 {
+    if apples <= 40 {
+        return apples * 2;
+    }
+    apples
 
+ }
 // Don't modify this function!
 #[test]
 fn verify_test() {
