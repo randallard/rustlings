56225b638c40d68369db0c5cff6b2c51068fc5f8
Ryan Khetlyr <ryan_khetlyr@waed.uscourts.gov>
2024-05-02 10:56:06 -0700

    almost through enums in rustlings

--- book/loops/src/main.rs
+++ book/loops/src/main.rs
@@ -1,15 +1,20 @@
 fn main() {
     println!("Hello, world!!!");
 
+// region:    --- returning values from loops
+    // https://rust-book.cs.brown.edu/ch03-05-control-flow.html#returning-values-from-loops
     let mut counter = 0;
     
     let result = loop {
         counter += 1;
-        println!("->> counter: {counter}");
+        println!("  ->> counter: {counter}");
         if counter == 10 {
             break counter * 2;
         }
     };
 
     println!("The result is {result}");
+// endregion: --- returning values from loops
+
+
 }
--- exercises/08_enums/enums3.rs
+++ exercises/08_enums/enums3.rs
@@ -8,7 +8,10 @@
 // I AM NOT DONE
 
 enum Message {
-    // TODO: implement the message variant types based on their usage below
+    Move( Point ),
+    Echo( String ),
+    ChangeColor( u8, u8, u8 ),
+    Quit,
 }
 
 struct Point {
@@ -44,6 +47,7 @@ impl State {
         // TODO: create a match expression to process the different message variants
         // Remember: When passing a tuple as a function argument, you'll need extra parentheses:
         // fn function((t, u, p, l, e))
+        todo!();
     }
 }
 
